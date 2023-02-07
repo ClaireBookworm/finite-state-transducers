169ceaef75d4afd86c734cb016b61add88367ac8
Michael Huang <michaelyhuang23@gmail.com>
2023-02-07 11:45:29 -0500

    bug fix

--- transducer/module.py
+++ transducer/module.py
@@ -1,9 +1,11 @@
 
-class Node:
-	def __init__(self,id_,isFinal_,transitions_=[]):
+class Point:
+	def __init__(self,id_,isFinal_,transitions_=None):
 		self.id = id_
 		self.isFinal = isFinal_
 		self.transitions = transitions_
+		if self.transitions is None:
+			self.transitions = []
 	def add_transition(self,transition=None):
 		if transition is not None:
 			self.transitions.append(transition)
@@ -11,6 +13,8 @@ class Node:
 		for transition in self.transitions:
 			if transition.contains(word):
 				return transition
+		raise SyntaxError('you have fucked up')
+		return None
 
 class Transition:
 	def __init__(self,words_,start_,target_,output_):
@@ -24,26 +28,29 @@ class Transition:
 class FiniteStateTranducer:
 	def __init__(self):
 		pass
-	def build(self,start_id_,clear_nodes,transitions):
+	def build(self,start_id_,clear_points,transitions):
 		self.start_id = start_id_
-		self.nodes = clear_nodes # a dict id:node
-		for id_,node in self.nodes.items():
+		self.points = clear_points # a dict id:point
+		for id_ in self.points.keys():
 			for transition in transitions:
 				if transition.start == id_:
-					node.add_transition(transition)
+					print(transition.start, transition.target, id_)
+					self.points[id_].add_transition(transition)
 	def step(self,word):
-		transition = self.nodes[self.start_id].get_transition(word)
+		transition = self.points[self.start_id].get_transition(word)
 		self.start_id = transition.target
-		return self.nodes[transition.target].isFinal, transition.output
+		return self.points[transition.target].isFinal, transition.output
 	def run(self,words):
 		outputs = []
 		end = False
 		for word in words:
 			end, output = self.step(word)
 			outputs.append(output)
-		return end # report if it's successful or not
+		return end, outputs # report if it's successful or not
 	def build_from_dict(self,machine):
-		clear_nodes = {i:Node(i,node['isAcceptState']) for i,node in enumerate(machine['nodes'])}
+		clear_points = {}
+		for i,point in enumerate(machine['nodes']):
+			clear_points[i] = Point(i, point['isAcceptState'])
 		transitions = []
 		start_id = 0
 		for link in machine['links']:
@@ -54,7 +61,7 @@ class FiniteStateTranducer:
 			if '`' in text:
 				input_, output_ = text.split('`')[1], text.split('`')[3]
 			elif ':' in text:
-				input_, output_ = text.split(':')[0][:-1], text.split(':')[1][1:]
+				input_, output_ = text.split(':')[0], text.split(':')[1]
 			else:
 				print('you are fucked')
 			if link['type']=='SelfLink':
@@ -63,7 +70,7 @@ class FiniteStateTranducer:
 				transitions.append(Transition(input_,link['nodeA'],link['nodeB'],output_))
 			else:
 				pass
-		self.build(start_id,clear_nodes,transitions)
+		self.build(start_id,clear_points,transitions)
 
 
 
--- transducer/test.py
+++ transducer/test.py
@@ -3,9 +3,12 @@ import json
 
 machine = FiniteStateTranducer()
 
-with open('../python-to-cpp/automaton_if.json') as f:
+with open('../python-to-cpp/automaton_test.json') as f:
 	obj = json.load(f)
 
 print(obj)
 machine.build_from_dict(obj)
 
+fit, outputs = machine.run(['a','b','c'])
+print(f'works? : {fit}')
+print(outputs)
